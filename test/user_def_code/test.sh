7063da3795744340b31caebb05ea3e551a7728cf
cheerayhuang <cheeray.huang@gmail.com>
2014-08-13 01:54:54 +0800

    [add] add test for running user-defined codes 	新文件:       ../../test/user_def_code/test.sh

---
+++ test/user_def_code/test.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' -d '{"rawdata": [{"key1": "curl_key1"}, {"property":"curl_property1"}, {"i_name": "curl_i_name1"}], "indexes": ["i_name", "property"]}' http://localhost:8088/http_test/Store/SaveObj
