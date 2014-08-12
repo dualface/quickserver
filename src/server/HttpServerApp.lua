1d84744f7d0c7dc832772eb213b6028585c24f7d
cheerayhuang <cheeray.huang@gmail.com>
2014-08-13 01:51:25 +0800

    [modify] handle request via HTTP 	新文件:       HttpServerApp.lua 	修改:         lib/HttpServerBase.lua

---
+++ src/server/HttpServerApp.lua
@@ -0,0 +1,42 @@
+local HttpServerApp = class("HttpServerApp", cc.server.HttpServerBase)
+
+function HttpServerApp:ctor(config)
+    HttpServerApp.super.ctor(self, config)
+
+    if self.config.debug then
+        print("---------------- START -----------------")
+        -- self:getComponent("components.behavior.EventProtocol"):setEventProtocolDebugEnabled(true)
+    end
+
+    self:addEventListener(HttpServerApp.CLIENT_ABORT_EVENT, self.onClientAbort, self)
+
+end
+
+function HttpServerApp:doRequest(actionName, data)
+    if self.config.debug then
+        --printLog("ACTION", ">> call [%s]", actionName)
+        echoInfo("ACTION >> call [%s]", actionName)
+    end
+
+    local _, result = xpcall(function()
+        return HttpServerApp.super.doRequest(self, actionName, data)
+    end, function(msg) return {error = msg} end)
+
+    if self.config.debug then
+        local j = json.encode(result)
+        --printLog("ACTION", "<< ret  [%s] = (%d bytes) %s", actionName, string.len(j), j)
+        echoInfo("ACTION << ret  [%s] = (%d bytes) %s", actionName, string.len(j), j)
+        --printLog("ACTION", "<<<<")
+        --echoInfo("ACTION <<")
+    end
+
+    return result
+end
+
+-- events callback
+-- dummy here
+function HttpServerApp:onClientAbort(event)
+
+end
+
+return HttpServerApp
--- src/server/lib/HttpServerBase.lua
+++ src/server/lib/HttpServerBase.lua
@@ -6,13 +6,24 @@ local HttpServerBase = class("HttpServerBase", ServerAppBase)
 function HttpServerBase:ctor(config)
     HttpServerBase.super.ctor(self, config)
 
-    self.config.requestType = "http"
-
+    self.requestType = "http"
+    self.uri = ngx.var.uri
     self.requestMethod = ngx.req.get_method()
     self.requestParameters = ngx.req.get_uri_args()
+
     if self.requestMethod == "POST" then
         ngx.req.read_body()
-        table.merge(self.requestParameters, ngx.req.get_post_args())
+        -- handle json body
+        local conType = ngx.req.get_headers(0)["Content-Type"]
+        if conType == "application/json" then
+            local body = json.decode(ngx.req.get_body_data())
+            --since 'table.merge' is implemented stupid, can't use here to merge a complex json table.
+            --TODO: need re-implement 'table.merge'
+            --table.merge(self.requestParameters, body)
+            self.requestParameters = body
+        else
+            table.merge(self.requestParameters, ngx.req.get_post_args())
+        end
     end
 
     local ok, err = ngx.on_abort(function()
@@ -28,7 +39,38 @@ function HttpServerBase:ctor(config)
 end
 
 function HttpServerBase:runEventLoop()
+    --ngx.say("call: "..self.uri)
+    --ngx.say("uri_prefix: " .. self.config.userDefinedCodes.uriPrefix)
 
+    local uri = self.uri
+    local prefixLen = string.len(self.config.userDefinedCodes.uriPrefix)
+    local rawAction = string.sub(uri, prefixLen+3, -1)
+    rawAction = string.gsub(rawAction, "/", ".")
+
+    echoInfo("requst via HTTP,  Action: %s", rawAction)
+    self:dumpParams()
+
+    local result = self:doRequest(rawAction, self.requestParameters)
+    if result and type(result) == "table" then
+        -- simple http rsp
+        ngx.say(json.encode(result))
+    end
+end
+
+-- for test
+function HttpServerBase:dumpParams()
+    echoInfo("DUMP HTTP params: %s", json.encode(self.requestParameters))
+    --[[
+    for k,v in pairs(self.requestParameters) do
+        if type(v) == "table" then
+            echoInfo("%s : %s", k, table.concat(v, ", "))
+            --ngx.say(k, " :", table.concat(v, ", "))
+        else
+            echoInfo("%s : %s", k, v)
+            --ngx.say(k, " :", v)
+        end
+    end
+    --]]
 end
 
 return HttpServerBase
