27b23cf1d7044385d4e5d9c9f2222a469d1143c0
cheerayhuang <cheeray.huang@gmail.com>
2014-08-13 01:53:00 +0800

    [modify] implment bootstrap for http server 	修改:         http_bootstrap.lua

--- src/server/http_bootstrap.lua
+++ src/server/http_bootstrap.lua
@@ -1,35 +1,8 @@
---[[require("server.lib.init")
+require("server.lib.init")
 require("server.lib.errors")
 
 -- create server app instance
 local config = require("server.config")
 local app = require("server.HttpServerApp").new(config)
 app:run()
---]]
 
-
-local function merge(dest, src) 
-    for k,v in pairs(src) do 
-        dest[k] = v
-    end
-end
-
-ngx.req.read_body()
-local args, err = ngx.req.get_post_args()
-if not args then
-    ngx.say("failed to get post args: ", err)
-    return
-end
-
-local args2, err = ngx.req.get_uri_args()
-if args2 then
-    merge(args, args2)
-end
-
-for key, val in pairs(args) do
-    if type(val) == "table" then
-        ngx.say(key, ": ", table.concat(val, ", "))
-    else
-        ngx.say(key, ": ", val)
-    end
-end
