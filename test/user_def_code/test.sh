9d7af4fc12669948f72db96fc358216797b3fd67
cheearyhuang <cheeray.huang@gmail.com>
2014-08-13 16:57:36 +0800

    [modify] add tests for committing and exec user-defined codes. 	modified:   test.sh

--- test/user_def_code/test.sh
+++ test/user_def_code/test.sh
@@ -1,1 +1,5 @@
-curl -H 'Content-Type: application/json' -d '{"rawdata": [{"key1": "curl_key1"}, {"property":"curl_property1"}, {"i_name": "curl_i_name1"}], "indexes": ["i_name", "property"]}' http://localhost:8088/http_test/Store/SaveObj
+#curl -H 'Content-Type: application/json' -d '{"rawdata": [{"key1": "curl_key1"}, {"property":"curl_property1"}, {"i_name": "curl_i_name1"}], "indexes": ["i_name", "property"]}' http://localhost:8088/http_test/Store/SaveObj
+
+curl -H 'Content-Type: application/json' -d '{"name":"a_name"}' http://localhost:8088/http_test/Say/SayHello
+curl http://localhost:8088/http_test/Say/SayHello?name=a_name
+curl -d 'name=a_name' http://localhost:8088/http_test/Say/SayHello
