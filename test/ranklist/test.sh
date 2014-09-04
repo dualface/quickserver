c1316d37bfb1e72bf1fdb36295bcf5371505f5a0
cheearyhuang <cheeray.huang@gmail.com>
2014-09-04 15:14:20 +0800

    [add] add test cases for accessing store & ranklist service via http 	new file:   test.sh 	new file:   ../store/test.sh

---
+++ test/ranklist/test.sh
@@ -0,0 +1,2 @@
+# cheeray-aliyun (115.28.88.113)
+curl http://cheeray-aliyun:8088/_ServER/ranklist/Count?ranklist=myzset
---
+++ test/store/test.sh
@@ -0,0 +1,2 @@
+# cheeray-aliyun (115.28.88.113)
+curl "http://cheeray-aliyun:8088/_SERVER/Store/findOBJ?property=i_name&property_value=0001"
