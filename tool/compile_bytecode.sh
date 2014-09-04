bf21d95e97b543bedb5d17a4712210d058ce9a5f
cheearyhuang <cheeray.huang@gmail.com>
2014-09-04 16:13:43 +0800

    [modify] rename "release_bytecodes.sh", and add some comments. 	new file:   compile_bytecode.sh 	deleted:    release_bytecodes.sh

---
+++ tool/compile_bytecode.sh
@@ -0,0 +1,16 @@
+#precondition: 
+# "lua" is the soft symbol link to luajit
+# "jit.*" modules from luajit(usual in /opt/quick_server/openresty/luajit/share/...) is in LUA_PATH or current dir.
+
+luajit=luajit-2.1.0-alpha
+sudo cp /opt/quick_server/openresty/luajit/share/$luajit/jit . -rf
+
+names=$(find ../src/ -name "*.lua")
+
+for name in $names 
+do
+    #echo $name
+    lua -b $name $name  
+done
+
+sudo rm ./jit -rf
--- tool/release_bytecodes.sh
+++
@@ -1,7 +0,0 @@
-names=$(find ../src/ -name "*.lua")
-
-for name in $names 
-do
-    #echo $name
-    lua -b $name $name  
-done
