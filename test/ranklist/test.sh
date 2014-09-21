4cf7f27e0ef6420f8e590e586701e52ad66ceaf2
cheerayhuang <cheeray.huang@gmail.com>
2014-09-22 00:15:46 +0800

    [add] add tests for user-login 	modify:         test/ranklist/test.html 	modify:         test/ranklist/test.sh 	modify:         test/store/test.html 	modify:         test/store/test.sh 	modify:         test/user_login/test.sh

--- test/ranklist/test.sh
+++ test/ranklist/test.sh
@@ -1,2 +1,15 @@
 # cheeray-aliyun (115.28.88.113)
-curl http://cheeray-aliyun:8088/_ServER/ranklist/Count?ranklist=myzset
+#curl http://cheeray-aliyun:8088/_ServER/ranklist/Count?ranklist=myzset
+
+#count
+curl 'http://localhost:8088/_ServER/rankList/count?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27'
+
+#add
+curl 'http://localhost:8088/_ServER/rankList/add?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a&value=11'
+
+#score 
+curl 'http://localhost:8088/_ServER/rankList/score?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a'
+
+#rank 
+curl 'http://localhost:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a' #del
+curl 'http://localhost:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a'
--- test/store/test.sh
+++ test/store/test.sh
@@ -1,2 +1,4 @@
 # cheeray-aliyun (115.28.88.113)
-curl "http://cheeray-aliyun:8088/_SERVER/Store/findOBJ?property=i_name&property_value=0001"
+#curl "http://cheeray-aliyun:8088/_SERVER/Store/findOBJ?property=i_name&property_value=0001"
+
+curl "http://127.0.0.1:8088/_SERVER/Store/findOBJ?property=i_name&property_value=curl_i_name1&session_id=00e0bfc67d3261a7c1dbc7e55bf14424"
