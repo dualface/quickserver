1efa8679f1739a0f6ff7a4222a4b8b769977e484
cheearyhuang <cheeray.huang@gmail.com>
2014-09-22 12:28:24 +0800

    [modify] add some tests for user-login 	modified:   test/ranklist/test.sh 	modified:   test/store/test.html 	modified:   test/store/test.sh

--- test/ranklist/test.sh
+++ test/ranklist/test.sh
@@ -2,14 +2,16 @@
 #curl http://cheeray-aliyun:8088/_ServER/ranklist/Count?ranklist=myzset
 
 #count
-curl 'http://localhost:8088/_ServER/rankList/count?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27'
+curl 'http://localhost:8088/_ServER/rankList/count?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572'
 
 #add
-curl 'http://localhost:8088/_ServER/rankList/add?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a&value=11'
+curl 'http://localhost:8088/_ServER/rankList/add?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a&value=11'
 
 #score 
-curl 'http://localhost:8088/_ServER/rankList/score?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a'
+curl 'http://localhost:8088/_ServER/rankList/score?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a'
 
 #rank 
-curl 'http://localhost:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a' #del
-curl 'http://localhost:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=44cb892f7ca141b6df7612e284feec27&key=a'
+curl 'http://localhost:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a' 
+
+#del
+curl 'http://localhost:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a'
