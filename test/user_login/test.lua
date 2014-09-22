69d310d5323280a8a62df1363762e260cc12457d
cheearyhuang <cheeray.huang@gmail.com>
2014-09-22 18:28:40 +0800

    [modify] add some tests for user-login function 	new file:   local_test.sh 	modified:   test.html 	modified:   ../user_login/test.lua

--- test/user_login/test.lua
+++ test/user_login/test.lua
@@ -5,5 +5,5 @@ url = require("resty.url")
 
 local str = [[password=9bb4aa9034e9b399b5a77a3a49e376bd&sign=61694F908853A4F40757182BB451A1DA&username=hqycxy&from=quickcocos2dx&timestamp=1411117923]]
 
-print(string.sub(url.escape(str))
+print(url.escape(str))
 
