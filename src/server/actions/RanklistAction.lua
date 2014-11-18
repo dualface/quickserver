606cb3bbb0fd0b782978e77ac6dfb3b5d31d9d88
cheearyhuang <cheeray.huang@gmail.com>
2014-11-18 16:55:55 +0800

    [modify] As "session_id" includes "username", don't need param "key" anymore. 	modified:   RanklistAction.lua

--- src/server/actions/RanklistAction.lua
+++ src/server/actions/RanklistAction.lua
@@ -76,7 +76,7 @@ function RankListAction:CountAction(data)
 end
 
 -- zadd
--- param: ranklist, key, value 
+-- param: ranklist, value 
 function RankListAction:AddAction(data)  
     assert(type(data) ==  "table", "data is NOT a table")
 
@@ -85,13 +85,13 @@ function RankListAction:AddAction(data)
         throw(ERR_SERVER_RANKLIST_ERROR, "ranklist object does NOT EXIST")
     end
  
-    if not CheckParams(data, "ranklist", "key", "value") then 
-        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist, key or value) are missed")
+    if not CheckParams(data, "ranklist", "value") then 
+        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist, value) are missed")
         return self.reply
     end 
 
     local listName = data.ranklist
-    local key = data.key
+    local key = data.__username
     local value = tonumber(data.value)
     if type(value) ~= "number" then 
         self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "param(value) is NOT number")
@@ -109,7 +109,7 @@ function RankListAction:AddAction(data)
 end
 
 -- zrem
--- param: ranklist, key
+-- param: ranklist
 function RankListAction:RemoveAction(data)
     assert(type(data) ==  "table", "data is NOT a table")
 
@@ -118,13 +118,13 @@ function RankListAction:RemoveAction(data)
         throw(ERR_SERVER_RANKLIST_ERROR, "ranklist object does NOT EXIST")
     end
  
-    if not CheckParams(data, "ranklist", "key") then 
-        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist or key) are missed")
+    if not CheckParams(data, "ranklist") then 
+        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist) are missed")
         return self.reply
     end
 
     local listName = data.ranklist
-    local key = data.key 
+    local key = data.__username
     local err = nil 
     ok, err = rl:command("zrem", listName, key) 
     if not ok then 
@@ -154,7 +154,7 @@ end
 --]]
 
 -- zscore
--- param: ranklist, key
+-- param: ranklist
 function RankListAction:ScoreAction(data) 
     assert(type(data) ==  "table", "data is NOT a table")
 
@@ -163,13 +163,13 @@ function RankListAction:ScoreAction(data)
         throw(ERR_SERVER_RANKLIST_ERROR, "ranklist object does NOT EXIST")
     end
  
-    if not CheckParams(data, "ranklist", "key") then 
-        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist or key) are missed")
+    if not CheckParams(data, "ranklist") then 
+        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist) are missed")
         return self.reply
     end
     
     local listName = data.ranklist
-    local key = data.key
+    local key = data.__username
     local score, err = rl:command("zscore", listName, key)
     if not score then 
         echoError("redis command zscore faild: %s", err)
@@ -234,7 +234,7 @@ function RankListAction:GetscorerangeAction(data)
 end
 
 -- zrank 
--- param: ranklist, key
+-- param: ranklist
 function RankListAction:GetrankAction(data) 
     assert(type(data) ==  "table", "data is NOT a table")
 
@@ -243,13 +243,13 @@ function RankListAction:GetrankAction(data)
         throw(ERR_SERVER_RANKLIST_ERROR, "ranklist object does NOT EXIST")
     end
  
-    if not CheckParams(data, "ranklist", "key") then 
-        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist or key) are missed")
+    if not CheckParams(data, "ranklist") then 
+        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist) are missed")
         return self.reply
     end
 
     local listName = data.ranklist
-    local key = data.key
+    local key = data.__username
     local rank, err = rl:command("zrank", listName, key)
     if not rank then 
         echoError("redis command zrank failed: %s", err)
@@ -265,7 +265,7 @@ function RankListAction:GetrankAction(data)
 end 
 
 -- zrevrank 
--- param: ranklist, key
+-- param: ranklist
 function RankListAction:GetrevrankAction(data) 
     assert(type(data) ==  "table", "data is NOT a table")
 
@@ -274,13 +274,13 @@ function RankListAction:GetrevrankAction(data)
         throw(ERR_SERVER_RANKLIST_ERROR, "ranklist object does NOT EXIST")
     end
  
-    if not CheckParams(data, "ranklist", "key") then 
-        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist or key) are missed")
+    if not CheckParams(data, "ranklist") then 
+        self.reply = Err(ERR_RANKLIST_INVALID_PARAM, "params(ranklist) are missed")
         return self.reply
     end
 
     local listName = data.ranklist
-    local key = data.key
+    local key = data.__username
     local rev_rank, err = rl:command("zrevrank", listName, key)
     if not rev_rank then 
         echoError("redis command zrevrank failed: %s", err)
