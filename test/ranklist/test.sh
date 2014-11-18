8fde43590e3837a23525b47e30752157c0e60858
cheearyhuang <cheeray.huang@gmail.com>
2014-11-18 17:57:05 +0800

    [modify] modify test cases 	modified:   test/ranklist/test.sh 	modified:   test/user_login/test.sh

--- test/ranklist/test.sh
+++ test/ranklist/test.sh
@@ -2,16 +2,22 @@
 #curl http://cheeray-aliyun:8088/_ServER/ranklist/Count?ranklist=myzset
 
 #count
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/count?ranklist=myzset&session_id=f68ac97d085338d3ef45ab19fffa3b57'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/count?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7'
 
 #add
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/add?ranklist=myzset&session_id=f68ac97d085338d3ef45ab19fffa3b57&key=a&value=11'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/add?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a&value=11'
 
 #score 
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/score?ranklist=myzset&session_id=f68ac97d085338d3ef45ab19fffa3b57&key=a'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/score?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a'
 
 #rank 
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=f68ac97d085338d3ef45ab19fffa3b57&key=a' 
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a' 
+
+#revrank
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrEvrank?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a' 
+
+#getrankrange
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrankrange?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&offset=1&count=10' 
 
 #del
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=f68ac97d085338d3ef45ab19fffa3b57&key=a'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a'
