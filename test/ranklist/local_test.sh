d8a41e2c499298b25ed07d17dc86de66a4d1df79
cheearyhuang <cheeray.huang@gmail.com>
2014-11-18 16:59:20 +0800

    [modify] modify test cases for user_login & ranklist actions. 	modified:   test/ranklist/local_test.sh 	modified:   test/user_login/local_test.sh

--- test/ranklist/local_test.sh
+++ test/ranklist/local_test.sh
@@ -2,16 +2,22 @@
 #curl http://cheeray-aliyun:8088/_ServER/ranklist/Count?ranklist=myzset
 
 #count
-curl 'http://localhost:8088/_ServER/rankList/count?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572'
+curl 'http://localhost:8088/_ServER/rankList/count?ranklist=myzset&session_id=690e1677184c3247d1234312230b1830'
 
 #add
-curl 'http://localhost:8088/_ServER/rankList/add?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a&value=11'
+curl 'http://localhost:8088/_ServER/rankList/add?ranklist=myzset&session_id=690e1677184c3247d1234312230b1830&key=a&value=11'
 
 #score 
-curl 'http://localhost:8088/_ServER/rankList/score?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a'
+curl 'http://localhost:8088/_ServER/rankList/score?ranklist=myzset&session_id=690e1677184c3247d1234312230b1830&key=a'
 
 #rank 
-curl 'http://localhost:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a' 
+curl 'http://localhost:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=690e1677184c3247d1234312230b1830&key=a' 
+
+#revrank
+curl 'http://localhost:8088/_ServER/rankList/GEtrEvrank?ranklist=myzset&session_id=690e1677184c3247d1234312230b1830&key=a' 
+
+#getrankrange
+curl 'http://localhost:8088/_ServER/rankList/GEtrankrange?ranklist=myzset&session_id=690e1677184c3247d1234312230b1830&offset=1&count=10' 
 
 #del
-curl 'http://localhost:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=9884982f83c0b8d58132d9d925e15572&key=a'
+curl 'http://localhost:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=690e1677184c3247d1234312230b1830&key=a'
