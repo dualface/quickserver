981fcc51160431a06653885f063c633174539267
cheearyhuang <cheeray.huang@gmail.com>
2014-11-25 13:08:11 +0800

    modified:   ranklist/test.sh 	modified:   user_login/test.sh

--- test/ranklist/test.sh
+++ test/ranklist/test.sh
@@ -2,22 +2,22 @@
 #curl http://cheeray-aliyun:8088/_ServER/ranklist/Count?ranklist=myzset
 
 #count
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/count?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/count?ranklist=myzset&session_id=7362ae8a10a3ea90ceffe3f5e98eacf7'
 
 #add
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/add?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a&value=11'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/add?ranklist=myzset&session_id=7362ae8a10a3ea90ceffe3f5e98eacf7&key=a&value=11'
 
 #score 
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/score?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/score?ranklist=myzset&session_id=7362ae8a10a3ea90ceffe3f5e98eacf7&key=a'
 
 #rank 
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a' 
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrank?ranklist=myzset&session_id=7362ae8a10a3ea90ceffe3f5e98eacf7&key=a' 
 
 #revrank
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrEvrank?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a' 
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrEvrank?ranklist=myzset&session_id=7362ae8a10a3ea90ceffe3f5e98eacf7&key=a' 
 
 #getrankrange
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrankrange?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&offset=1&count=10' 
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/GEtrankrange?ranklist=myzset&session_id=7362ae8a10a3ea90ceffe3f5e98eacf7&offset=1&count=10' 
 
 #del
-curl 'http://cheeray-aliyun:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=ad1fafe255b765054daf65761fd930f7&key=a'
+curl 'http://cheeray-aliyun:8088/_ServER/rankList/reMove?ranklist=myzset&session_id=7362ae8a10a3ea90ceffe3f5e98eacf7&key=junk_message!!!'
