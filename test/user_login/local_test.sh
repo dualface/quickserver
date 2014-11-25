a156fa6a4b5b83a887108a8e817bf9e58fd0e555
cheearyhuang <cheeray.huang@gmail.com>
2014-11-25 13:21:36 +0800

    [modify] modify test cases for user login 	modified:   ../user_login/local_test.sh 	modified:   ../user_login/test.sh

--- test/user_login/local_test.sh
+++ test/user_login/local_test.sh
@@ -1,3 +1,2 @@
-curl  -H 'Content-Type: application/json' -d '{"user":"hqycxy"}' http://localhost:8088/_server/user/Login
+curl  -H 'Content-Type: application/json' -d '{"id":"hhh"}' http://localhost:8088/_server/user/session
 
-#curl -H 'Content-Type: application/x-www-form-urlencoded' http://open.cocoachina.com/api/user_login -d 'password=9bb4aa9034e9b399b5a77a3a49e376bd&sign=1FADC2A39436B716229343D05C267528&username=hqycxy&from=quickcocos2dx&timestamp=1411121032'
