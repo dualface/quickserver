6a6ca7b8b149dc856f3acbf13d1ae6a20bdd4f96
cheerayhuang <cheeray.huang@gmail.com>
2014-12-27 16:25:13 +0800

    [modify] add a simple hello world demo. 	rename:   src/server/actions/HiAction.lua -> src/server/actions/HelloworldAction.lua 	delete:     src/server/actions/SayAction.lua

--- src/server/actions/HelloworldAction.lua
+++ src/server/actions/HelloworldAction.lua
@@ -1,10 +1,10 @@
 
 -- define action obj, notice its name should be same to file name. 
-local HiAction = class("HiAction", cc.server.ActionBase)
+local HelloworldAction = class("HelloworldAction", cc.server.ActionBase)
 
 -- define actions, param 'data' is necessary. 
--- it means you can call this interface via uri '/prefix/Hi/SayHello' 
-function HiAction:SayhelloAction(data)
+-- it means you can call this interface via uri '../Helloworld/SayHello' 
+function HelloworldAction:SayhelloAction(data)
 
     -- use json lib 
     local jsonStr = json.encode(data)
@@ -12,24 +12,24 @@ function HiAction:SayhelloAction(data)
 end
 
 -- another action
-function HiAction:SayhiAction(data)
+function HelloworldAction:SayhiAction(data)
     
     return {dont_want_say_hi = data.name}
 end
 
 -- you can write other func
-function HiAction:SayFunc(jsonStr)
+function HelloworldAction:SayFunc(jsonStr)
     local tbl = json.decode(jsonStr)
 
     -- return a table
-    return {real_hi = tbl.name}
+    return {hello_world = tbl.name}
 end
 
 -- you can write more functions and codes, according to your requirement.
-function HiAction:Funcs()
+function HelloworldAction:Funcs()
 end
 
 local function Funcs()
 end
 
-return HiAction
+return HelloworldAction
