8417750076c90e2e7662c317cd4e24a7cef2b40c
cheerayhuang <cheeray.huang@gmail.com>
2015-01-12 23:03:02 +0800

    [modify] fix some path name errors. 	modified:   start_quick_server.sh

--- tool/start_quick_server.sh
+++ tool/start_quick_server.sh
@@ -1,6 +1,6 @@
 CURRDIR=$(dirname $(readlink -f $0))
 NGINXDIR=$CURRDIR/openresty/nginx/
-SRCDIR=$CURRDIR/openresty/packages/objectstorage/tools/
+SRCDIR=$CURRDIR/openresty/packages/objectstorage/tool/
 
 $CURRDIR/redis/bin/redis-server $CURRDIR/conf/redis.conf
 
@@ -10,6 +10,6 @@ cd $NGINXDIR
 . ./sbin/start.sh
 
 cd $SRCDIR
-. ./cleaner.sh  > /dev/null 2> $NGINXDIR/log/cleaner.log &
+. ./cleaner.sh  > /dev/null 2> $NGINXDIR/logs/cleaner.log &
 
 cd $CURRDIR
