952ec4cc1d0b268980e6dac37dce7e8487dbe59d
cheerayhuang <cheeray.huang@gmail.com>
2015-01-14 15:23:44 +0800

    [modfiy] show how to implement a action which inherits two package actions. 	modified:   server/actions/examples/TestAction.lua

--- src/server/actions/examples/TestAction.lua
+++ src/server/actions/examples/TestAction.lua
@@ -1,9 +1,12 @@
-local LeaderboardAction = cc.load("leaderboard").action
+local leaderboardAction = cc.load("leaderboard").action
+local storageActon = cc.load("objectstorage").action
 
-local TestAction = class("TestAction", LeaderboardAction) 
+local TestAction = class("TestAction", storageActon, leaderboardAction)
 
 function TestAction:ctor(app)
-    self.super:ctor(app)
+    for _, s in pairs(self.__supers) do
+        s:ctor(app)
+    end
 end 
 
 return TestAction
