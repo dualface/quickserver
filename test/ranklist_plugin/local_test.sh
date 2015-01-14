5a46e3dfad7592b2a3f01ea68f6aed5a30a8f63b
cheerayhuang <cheeray.huang@gmail.com>
2015-01-14 22:48:43 +0800

    [modify] clear unnecessary tests.

--- test/ranklist_plugin/local_test.sh
+++
@@ -1,4 +0,0 @@
-#call /user/codes update user-defined file
-
-curl -H 'Content-Type: application/json' -d '{"session_id": "2457082fdd65ab4b35fd2f011b2b953a", "name":"ranklist_plugin"}' http://127.0.0.1:8088/_server/helloworld/addandcount
-
--- test/user_login/local_test.sh
+++
@@ -1,2 +0,0 @@
-curl  -H 'Content-Type: application/json' -d '{"id":"hhh"}' http://localhost:8088/_server/user/session
-
