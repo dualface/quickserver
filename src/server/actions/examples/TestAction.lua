55603940e540de8f27d8311339a6a5ce24617740
cheerayhuang <cheeray.huang@gmail.com>
2015-01-19 01:08:24 +0800

    [modify] fix misspelling, and add "sayhello" action. 	modified:   server/actions/examples/TestAction.lua

--- src/server/actions/examples/TestAction.lua
+++ src/server/actions/examples/TestAction.lua
@@ -1,14 +1,18 @@
 local leaderboardAction = cc.load("leaderboard").action
-local storageActon = cc.load("objectstorage").action
+local storageAction = cc.load("objectstorage").action
 local chatAction = cc.load("chat").action
 local jobAction = cc.load("job").action
 
-local TestAction = class("TestAction", storageActon, leaderboardAction, chatAction, jobAction)
+local TestAction = class("TestAction", storageAction, leaderboardAction, chatAction, jobAction)
 
 function TestAction:ctor(app)
     for _, s in pairs(self.__supers) do
         s:ctor(app)
     end
 end 
+
+function TestAction:sayhelloAction(data)
+    return {hello = data.name}
+end
 
 return TestAction
