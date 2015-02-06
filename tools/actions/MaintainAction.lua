135a01a570e027718b2867a08ceba596d71bbaf6
cheearyhuang <cheeray.huang@gmail.com>
2015-02-06 09:25:45 +0800

    [modify] implement monitor action. 	modified:   tools/actions/MaintainAction.lua

--- tools/actions/MaintainAction.lua
+++ tools/actions/MaintainAction.lua
@@ -23,7 +23,13 @@ THE SOFTWARE.
 ]]
 
 local string_format = string.format
-local io
+local string_split = string.split
+local io_popen = io.popen
+
+local _GET_PID_PATTERN = "pgrep %s"
+local _GET_CPU_PATTERN = "ps -p %s -o pcpu=" 
+local _GET_MEM_PATTERN = "ps -p %s -o pmem="
+
 
 local MaintainAction = class("Maintain") 
 
@@ -31,22 +37,33 @@ function MaintainAction:ctor(app)
     self._app = app 
     self.config.process = self.config.monitor.process
 
-    self._pid = {}
-    self._data = {}
+    self._procData = {}
 end
 
 function MaintainAction:monitorAction(arg)
     _getPid()
-    
+    _getPerfomance() 
+    _save()
 end
 
-function MaintainAction:_getPid(process)
+function MaintainAction:_save()
+end
+
+function MaintainAction:_getPerfomance()
+end
+
+function MaintainAction:_getPid()
+    local process = self.config.process
     for _, procName in ipairs(process) do
         local cmd = string_format("pgrep %s", procName)
         local fout = io_popen(cmd)
         local res = fout:read("*a")
         fout:close()
-        self._pid[procName] = string_split(res, "\n")
+
+        local pids = string_split(res, "\n")
+        for _, pid in ipairs(pids) do
+            self._procData[pid] = {}
+        end
     end
 end
 
