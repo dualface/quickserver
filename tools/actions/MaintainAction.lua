23ffed8b073535c90a7cb7d1d4fb140defa6f99d
cheearyhuang <cheeray.huang@gmail.com>
2015-02-10 18:05:31 +0800

    [modify] implement monitor tool. 	modified:   tools/actions/MaintainAction.lua 	renamed:    tools.sh -> tools/tools.sh

--- tools/actions/MaintainAction.lua
+++ tools/actions/MaintainAction.lua
@@ -27,9 +27,15 @@ local string_split = string.split
 local io_popen = io.popen
 
 local _GET_PID_PATTERN = "pgrep %s"
-local _GET_CPU_PATTERN = "ps -p %s -o pcpu=" 
-local _GET_MEM_PATTERN = "ps -p %s -o pmem="
+local _GET_PERFORMANCE_PATTERN = "ps -p %s -o pcpu= -o pmem=" 
 
+local _MONITOR_PROC_DICT_KEY = "_MONITOR_PROC_DICT"
+local _MONITOR_CPU_LIST_PATTERN = "_MONITOR_%s_CPU"
+local _MONITOR_MEM_LIST_PATTERN = "_MONITOR_%s_MEM"
+
+-- since this tool is running background as a loop,
+-- redis connection don't need closing.
+local RedisService = cc.load("redis").service
 
 local MaintainAction = class("Maintain") 
 
@@ -38,6 +44,7 @@ function MaintainAction:ctor(app)
     self.config.process = self.config.monitor.process
 
     self._procData = {}
+    self._isProcessRestart = false
 end
 
 function MaintainAction:monitorAction(arg)
@@ -47,13 +54,32 @@ function MaintainAction:monitorAction(arg)
 end
 
 function MaintainAction:_save()
+    local pipe = self:_getRedis():newPipeline()
+    for k, v in pairs(self._procData) do    
+        local cpuList = string_format(_MONITOR_CPU_LIST_PATTERN, "")
+        local memList = string_format(_MONITOR_MEM_LIST_PATTERN, "")
+        pipe:command("") 
+    end
+    pipe:commit()
 end
 
 function MaintainAction:_getPerfomance()
+    for k, _ in pairs(self._procData) do
+        local cmd = string_format(_GET_PERFORMANCE_PATTERN, k)
+        local fout = io_popen(cmd)
+        local res = string_sub(fout:read("*a"), 1, -2) -- trim "\n"
+        self._procData[k] = string_split(res, " ")  
+    end
 end
 
 function MaintainAction:_getPid()
+    if not self._isProcessRestart then
+        return 
+    end
+
     local process = self.config.process
+    local pipe = self._getRedis():newPipeline()
+
     for _, procName in ipairs(process) do
         local cmd = string_format("pgrep %s", procName)
         local fout = io_popen(cmd)
@@ -63,8 +89,29 @@ function MaintainAction:_getPid()
         local pids = string_split(res, "\n")
         for _, pid in ipairs(pids) do
             self._procData[pid] = {}
+            pipe:command("HSET", _MONITOR_PROC_DICT_KEY, pid, procName) 
         end
     end
+
+    pipe:commit()
+
+    self._isProcessRestart = false
+end
+
+function MaintainAction:_getRedis()
+    if not self._redis then
+        self._redis = self:_newRedis()
+    end
+    return self._redis
+end
+
+function MaintainAction:_newRedis()
+    local redis = RedisService:create(self.config.redis)
+    local ok, err = redis:connect()
+    if err then
+        throw("connect internal redis failed, %s", err)
+    end
+    return redis
 end
 
 return MaintainAction
