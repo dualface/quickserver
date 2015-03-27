4f331e51bfb3254591ad6d740d4668ef637c8b64
cheearyhuang <cheeray.huang@gmail.com>
2015-03-27 18:12:12 +0800

    Fix some bugs in stop_quick_server.sh
    - Before killing redis, it needs a loop to check whether nginx process
    is killed. if not, kill nginx again.
    - it should kill monitor first, cause monitor can reset other process.
    - a correct lua path should be offered, when using "killall" to kill monitor.

--- shells/stop_quick_server.sh
+++ shells/stop_quick_server.sh
@@ -16,7 +16,7 @@ function showHelp()
 function getNginxNumOfWorker()
 {
     LUABIN=$1/bin/openresty/luajit/bin/lua
-    CODE='_C=require("conf.config"); print(_C.numOfWorkers);'
+    CODE="package.path = [[$1/?.lua;]] .. package.path; _C=require([[conf.config]]); print(_C.numOfWorkers);"
 
     $LUABIN -e "$CODE"
 }
@@ -24,7 +24,7 @@ function getNginxNumOfWorker()
 function getNginxPort()
 {
     LUABIN=$1/bin/openresty/luajit/bin/lua
-    CODE='_C=require("conf.config"); print(_C.port);'
+    CODE="package.path = [[$1/?.lua;]] .. package.path; _C=require([[conf.config]]); print(_C.port);"
 
     $LUABIN -e "$CODE"
 }
@@ -93,19 +93,23 @@ if [ $RELOAD -ne 0 ]; then
     ALL=0
 fi
 
+# stop monitor first.
+killall tools.sh > /dev/null 2> /dev/null
+killall bin/openresty/luajit/bin/lua > /dev/null 2> /dev/null
+
 #stop nginx
 if [ $ALL -eq 1 ] || [ $NGINX -eq 1 ] || [ $RELOAD -eq 1 ]; then
     if [ $RELOAD -eq 0 ] ; then
         pgrep nginx > /dev/null
-        while [ $? -eq 0 ]
-        do
+        if [ $? -eq 0 ]; then
             nginx -q -p $CURRDIR -c $NGINXDIR/conf/nginx.conf -s stop
             if [ $? -ne 0 ]; then
                 exit $?
             fi
-            echo "Stop Nginx DONE"
-            pgrep nginx > /dev/null
-        done
+        fi
+
+        sleep 1
+        echo "Stop Nginx DONE"
     else
         PORT=$(getNginxPort $CURRDIR)
         sed -i "s#listen [0-9]*#listen $PORT#g" $NGINXDIR/conf/nginx.conf
@@ -120,6 +124,14 @@ fi
 
 #stop redis
 if [ $ALL -eq 1 ] || [ $REDIS -eq 1 ]; then
+    pgrep nginx > /dev/null
+
+    while [ $? -eq 0 ];
+    do
+        killall nginx 2> /dev/null
+        pgrep nginx > /dev/null
+    done
+
     killall redis-server 2> /dev/null
     echo "Stop Redis DONE"
 fi
@@ -130,8 +142,6 @@ if [ $ALL -eq 1 ] || [ $BEANS -eq 1 ]; then
     echo "Stop Beanstalkd DONE"
 fi
 
-killall tools.sh 2> /dev/null
-killall bin/openresty/luajit/bin/lua 2> /dev/null
 
 if [ $RELOAD -ne 0 ]; then
     $CURRDIR/tools.sh monitor.watch > $CURRDIR/logs/monitor.log &
