c607d65650ea29c36d5d318c59871804ced11b1b
non exist <non_exist@outlook.com>
2015-04-04 19:51:58 +0800

    Let "getopt_long" to support "--debug" and "--reload".

--- shells/src/getopt_long.c
+++ shells/src/getopt_long.c
@@ -5,6 +5,9 @@
 
 #define K_PREFIX 10000
 
+static int k_debug = 0;
+static int k_reload = 0;
+
 static struct option long_options[] = {
     /* These options set a flag. */
     {"all", no_argument, 0, 'a'},
@@ -12,7 +15,10 @@ static struct option long_options[] = {
     {"redis", no_argument, 0, 'r'},
     {"beanstalkd", no_argument, 0, 'b'},
     {"help", no_argument, 0, 'h'},
-    {"prefix", required_argument, 0, K_PREFIX}, 
+    {"version", no_argument, 0, 'v'},
+    {"debug", no_argument, &k_debug, 1},
+    {"reload", no_argument, &k_reload, 1},
+    {"prefix", required_argument, 0, K_PREFIX},
     {0, 0, 0, 0}
 };
 
@@ -25,7 +31,7 @@ char* save_option(char **opts, char c, int index, unsigned* len, unsigned* max_l
     }
     else {
         char tmp[4] = {' ', '-', c, '\0'};
-        strcpy(opt, tmp);  
+        strcpy(opt, tmp);
     }
 
     if (*len + strlen(opt) + 1 > *max_len) {
@@ -50,7 +56,7 @@ int main (int argc, char **argv) {
     unsigned int max_opt_len = 0;
 
     while(1) {
-        
+
         int option_index = -1;
 
         c = getopt_long (argc, argv, "abrnh", long_options, &option_index);
@@ -63,10 +69,15 @@ int main (int argc, char **argv) {
                 install_path = optarg;
                 break;
 
+            /* handle "--debug" and "--reload" */
+            case 0:
+                break;
+
             case 'a':
             case 'b':
             case 'r':
             case 'n':
+            case 'v':
             case 'h':
                 if (!save_option(&options, c, option_index, &cur_opt_len, &max_opt_len)) {
                     free(options);
@@ -75,7 +86,7 @@ int main (int argc, char **argv) {
                 break;
 
             case '?':
-                free(options); 
+                free(options);
                 exit(-1);
 
             default:
@@ -84,10 +95,22 @@ int main (int argc, char **argv) {
     }
 
     /* output args as command "getopt". */
-    printf("%s ", options);
+    if (options) {
+        printf("%s ", options);
+    }
+
     if (install_path) {
         printf("--prefix %s ", install_path);
     }
+
+    if (k_debug) {
+        printf("--debug ");
+    }
+
+    if (k_reload) {
+        printf("--reload");
+    }
+
     printf("--");
 
     free(options);
