31b55de53ffae9c7e41c9f9a95f3c7f05e7e9b73
non exist <non_exist@outlook.com>
2015-04-15 18:30:28 +0800

    Revert "Change formats in var ENV."
    This reverts commit 8773b5978fe5001c3766389519644d5fc20b1397.

--- shells/tools.sh
+++ shells/tools.sh
@@ -7,6 +7,6 @@ SCRIPT=CLIBootstrap.lua
 
 cd $QUICK_SERVER_ROOT
 
-ENV="SERVER_CONFIG = loadfile([[_QUICK_SERVER_ROOT_/conf/config.lua]])(); DEBUG = _DBG_DEBUG; require([[framework.init]]); SERVER_CONFIG.appRootPath = SERVER_CONFIG.appRootPath .. [[/tools/?.lua;]] .. SERVER_CONFIG.appRootPath;"
+ENV="SERVER_CONFIG=loadfile([[_QUICK_SERVER_ROOT_/conf/config.lua]])();DEBUG=_DBG_DEBUG;require([[framework.init]]);SERVER_CONFIG.appRootPath=SERVER_CONFIG.appRootPath..[[/tools]];"
 
 $LUABIN -e "$ENV" $QUICK_SERVER_ROOT/src/$SCRIPT $*
